28590152ed0f323372e963f7a6fe738c24857a8c
KyungHun Jung <56815709+Ruderud@users.noreply.github.com>
2025-04-23 20:17:40 +0900

    install lightweight chart & config simple chart (#2)
    * chore: install lightweight-chart
    
    * feat: create base chart comp
    
    * feat: lightweight 기본 챠트 사용 컴포넌트 추가, 메인화면 수정, component path 추가, /home을 처음화면으로 쓰도록 조정
    
    ---------
    
    Co-authored-by: 정경훈 <ruderud0055@gmail.com>

--- apps/neoavath/src/app/app.tsx
+++ apps/neoavath/src/app/app.tsx
@@ -1,31 +1,29 @@
-import NxWelcome from './nx-welcome';
+import { Route, Routes, Link, Navigate } from 'react-router-dom';
+import { Chart } from '@/components/Chart';
 
-import { Route, Routes, Link } from 'react-router-dom';
+const data = [
+  { time: '2019-04-11', value: 80.01 },
+  { time: '2019-04-12', value: 96.63 },
+  { time: '2019-04-13', value: 76.64 },
+  { time: '2019-04-14', value: 81.89 },
+  { time: '2019-04-15', value: 74.43 },
+  { time: '2019-04-16', value: 80.01 },
+  { time: '2019-04-17', value: 96.63 },
+  { time: '2019-04-18', value: 76.64 },
+  { time: '2019-04-19', value: 81.89 },
+  { time: '2019-04-20', value: 74.43 },
+];
 
-export function App() {
+export const App = () => {
   return (
-    <div>
-      <NxWelcome title="neoavath" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
+    <>
+      <nav>
+        <Link to="/home">Home</Link>
+        <Link to="/page-2">Page 2</Link>
+      </nav>
       <Routes>
         <Route
-          path="/"
+          path="/home"
           element={
             <div>
               This is the generated root route.{' '}
@@ -38,13 +36,16 @@ export function App() {
           element={
             <div>
               <Link to="/">Click here to go back to root page.</Link>
+
+              <Chart data={data} />
             </div>
           }
         />
+
+        <Route path="/" element={<Navigate to="/home" replace />} />
       </Routes>
-      {/* END: routes */}
-    </div>
+    </>
   );
-}
+};
 
 export default App;
