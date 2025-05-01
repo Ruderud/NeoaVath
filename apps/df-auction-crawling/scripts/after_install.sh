87b80f7613fa3f4c008923d52edc4852c34d508d
정경훈 <ruderud0055@gmail.com>
2025-05-01 15:51:29 +0900

    fix: build command fix

--- apps/df-auction-crawling/scripts/after_install.sh
+++ apps/df-auction-crawling/scripts/after_install.sh
@@ -2,7 +2,7 @@
 # 의존성 설치 및 빌드
 cd /home/ubuntu/app
 pnpm install --no-frozen-lockfile
-pnpm nx build:$DEPLOYMENT_GROUP_NAME
+pnpm build:$DEPLOYMENT_GROUP_NAME
 
 # 빌드 성공 여부 확인
 if [ $? -eq 0 ]; then
