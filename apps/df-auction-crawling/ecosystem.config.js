67699d972a65dc43df8c3140cd49de002aa932b3
정경훈 <ruderud0055@gmail.com>
2025-05-02 21:11:59 +0900

    test: deploy test4

--- apps/df-auction-crawling/ecosystem.config.js
+++ apps/df-auction-crawling/ecosystem.config.js
@@ -2,13 +2,15 @@ module.exports = {
   apps: [
     {
       name: 'df-auction-crawling',
-      script: 'dist/main.js',
+      script: 'node',
+      args: 'dist/main.js',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
       env: {
         NODE_ENV: 'production',
+        PORT: 3000,
       },
       error_file: 'logs/err.log',
       out_file: 'logs/out.log',
